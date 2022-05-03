c4cb3580244f9af343d3f62e202490f87b481eaa
cse20-018 <cse20-018@thuto.bac.ac.bw>
2022-05-03 02:27:09 +0200

    Done for now

--- app/src/main/java/com/diceGame/mygame/MainActivity.java
+++ app/src/main/java/com/diceGame/mygame/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button b_roll;
     TextView tv_rollNumber;
-    ImageView iv_dice;
+    ImageView iv_dice, iv_dice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +25,30 @@ public class MainActivity extends AppCompatActivity {
         b_roll = (Button) findViewById(R.id.b_roll);
         tv_rollNumber = (TextView) findViewById(R.id.tv_rollNumber);
         iv_dice = (ImageView) findViewById(R.id.iv_dice);
+        iv_dice2 = (ImageView) findViewById(R.id.iv_dice2);
 
         b_roll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Random r = new Random();
-                // The random number betweeen 1 and 6
+                // The random number between 1 and 6
                 int theRoll = r.nextInt(6) + 1;
-                tv_rollNumber.setText("You rolled a " + theRoll);
+                int theRoll2 = r.nextInt(6) + 1;
+                int sum = theRoll + theRoll2;
+                tv_rollNumber.setText("You rolled " + theRoll + " and  " + theRoll2 + "\nTotal: " + sum);
 
-                switch(theRoll){
-                    case 1:{
+
+                switch (theRoll) {
+                    case 1: {
                         iv_dice.setImageResource(R.drawable.dice1);
                         break;
                     }
-                    case 2:{
+                    case 2: {
                         iv_dice.setImageResource(R.drawable.dice2);
                         break;
                     }
-                    case 3:{
+
+                    case 3: {
                         iv_dice.setImageResource(R.drawable.dice3);
                         break;
                     }
@@ -57,6 +62,34 @@ public class MainActivity extends AppCompatActivity {
                     }
                     case 6: {
                         iv_dice.setImageResource(R.drawable.dice6);
+                        break;
+                    }
+                }
+
+                switch (theRoll2) {
+                    case 1: {
+                        iv_dice2.setImageResource(R.drawable.dice1);
+                        break;
+                    }
+                    case 2: {
+                        iv_dice2.setImageResource(R.drawable.dice2);
+                        break;
+                    }
+
+                    case 3: {
+                        iv_dice2.setImageResource(R.drawable.dice3);
+                        break;
+                    }
+                    case 4: {
+                        iv_dice2.setImageResource(R.drawable.dice4);
+                        break;
+                    }
+                    case 5: {
+                        iv_dice2.setImageResource(R.drawable.dice5);
+                        break;
+                    }
+                    case 6: {
+                        iv_dice2.setImageResource(R.drawable.dice6);
                         break;
                     }
                 }
